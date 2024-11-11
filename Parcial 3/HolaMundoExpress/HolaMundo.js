d28e03de8d55d50c457a578014a5c871a1bbf682
Emilio <chilaquilelperro205@gmail.com>
2024-11-10 22:03:19 -0600

    Peticiones Update 1.4.5

--- Parcial 3/HolaMundoExpress/HolaMundo.js
+++ Parcial 3/HolaMundoExpress/HolaMundo.js
@@ -1,12 +1,36 @@
 const express = require('express')
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Como cuando')
-}
-)
+// app.use((req, res, next)=>{
+//     res.send() //Le contestas al cliente, termina el ciclo
+//     next() //Realiza la que sigue
+//  })
+
+app.use(express.json());
+app.use(express.text());
+
+// app.use((req, res, next)=>{next()},
+//          (req, res, next)=>{next()},
+//          (req, res, next)=>{next()}
+// )
+
+app.get('/Admin', (req, res) => {
+    console.log(req.body)
+
+    res.send('Hola Minatitlan, para el mundo')
+})
+
+app.get('/Maestros', (req, res) => {
+    console.log(req.query)
+
+    res.send('Hola Minatitlan, para el mundo')
+})
+
+app.get('/Estudiantes/:Carrera', (req, res) => {
+    console.log(req.params)
+    res.send('Hola Minatitlan, para el mundo')
+})
 
 app.listen(8082, () => {
-    console.log(`Hola soy una peticion en npm 🤯: ${8082}`)
-}
-)
+    console.log(`Hola soy una peticion en npm: ${8082}`)
+})
